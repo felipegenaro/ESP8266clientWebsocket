affa3f503b2b3fb93a29640b2af2df94ce6284f2
Felipe Corniani de Genaro <felipegenaro@FelipeGenaroAir.home>
2018-06-29 00:37:59 -0300

    added target command

--- clientWebsocket.ino
+++ clientWebsocket.ino
@@ -1,29 +1,26 @@
 #include <Arduino.h>
-#include <Thread.h>
-#include <StaticThreadController.h>
 #include <ESP8266WiFi.h>
 #include <WebSocketsClient.h>
 #include <Hash.h>
+#include <Thread.h>
+#include <StaticThreadController.h>
 // #include <WiFiManager.h>
 
+WebSocketsClient webSocket;
 // WiFiManager wifiManager;
 
-//#define MAX_SRV_CLIENTS 1
-//
-//WiFiServer server(23);
-//WiFiClient serverClients[MAX_SRV_CLIENTS];
-
-WebSocketsClient webSocket;
-#define PIN_BUTTON D2
-String owner="Teste2";
-int buttonState=0;
+String owner = "MQ1";
+byte mac[6];
 
 //configuracao de rede
-const char* ssid     = "WIFI_EDUC_126";
-const char* password = "educ126634#";
+const char* ssid     = "<NETWORK>";
+const char* password = "<PASSWORD>";
 
 //set custom ip
 // wifiManager.setSTAStaticIPConfig(IPAddress(192,168,0,99), IPAddress(192,168,0,1), IPAddress(255,255,255,0));
+
+//definicao dos pinos
+#define PINO_LED D2
 
 #define USE_SERIAL Serial
 
@@ -32,21 +29,19 @@ Thread threadwebSocket = Thread();
 Thread threadRead = Thread();
 StaticThreadController<2> controll(&threadwebSocket,&threadRead);
  
-boolean power;
- 
-void webSocketRun(){
+void webSocketRun() {
     webSocket.loop();
-  }
+}
 void webSocketEvent(WStype_t type, uint8_t * payload, size_t lenght) {
-    webSocket.sendTXT("Owner= "+ owner + ", MAC= " + WiFi.macAddress());
+    WiFi.macAddress(mac);
+    int cMac = mac[5]+mac[4]+mac[3]+mac[2]+mac[1]+mac[0];
+  
+    webSocket.sendTXT("Owner= "+ owner + ", MAC= " + mac[5]+":"+mac[4]+":"+mac[3]+":"+mac[2]+":"+mac[1]+":"+mac[0]);
 
-    if(payload[0]=='s'){
-        digitalWrite(PIN_BUTTON, !digitalRead(PIN_BUTTON));
-//digitalWrite(LED_BUILTIN, LOW);
-//delay(500);
-//digitalWrite(LED_BUILTIN, HIGH);
-          
-//        ESP.reset();
+    //target commands
+    if (payload[0] == cMac) {
+        digitalWrite(PINO_LED, !digitalRead(PINO_LED));
+        // ESP.reset();
     }
 }
 
@@ -56,91 +51,27 @@ void setup() {
  
     WiFi.begin(ssid, password);
  
-    while(WiFi.status() != WL_CONNECTED) {
+    while (WiFi.status() != WL_CONNECTED) {
         delay(100);
     }
     
-    pinMode(PIN_BUTTON, OUTPUT);
+    pinMode(PINO_LED, OUTPUT);
     
     //thread de conexao
     threadwebSocket.onRun(webSocketRun);
     threadwebSocket.setInterval(1);
 
     //ip e porta do servidor
-    webSocket.begin("10.84.44.13", 81);
+    webSocket.begin("<HOST>", 81);
     webSocket.onEvent(webSocketEvent);
 
     WiFi.setSleepMode(WIFI_NONE_SLEEP);
-
-//    //start UART and the server
-//  Serial.begin(115200);
-//  server.begin();
-//  server.setNoDelay(true);
-//
-//  Serial1.print("Ready! Use 'telnet ");
-//  Serial1.print(WiFi.localIP());
-//  Serial1.println(" 23' to connect");
-
-//    delay(1000);
 }
  
 void loop() {
   controll.run();
 
   //envio de informacoes
-  webSocket.sendTXT("Owner= "+ owner + ", someData= aloaloalo");
-  delay(1000);
-  webSocket.sendTXT("Owner= "+ owner + ", someData= aloaloalo");
-  delay(1000);
-  webSocket.sendTXT("overLoad");
-  delay(1000);
-
-
-//  uint8_t i;
-//  //check if there are any new clients
-//  if (server.hasClient()) {
-//    for (i = 0; i < MAX_SRV_CLIENTS; i++) {
-//      //find free/disconnected spot
-//      if (!serverClients[i] || !serverClients[i].connected()) {
-//        if (serverClients[i]) {
-//          serverClients[i].stop();
-//        }
-//        serverClients[i] = server.available();
-//        Serial1.print("New client: "); Serial1.print(i);
-//        break;
-//      }
-//    }
-//    //no free/disconnected spot so reject
-//    if (i == MAX_SRV_CLIENTS) {
-//      WiFiClient serverClient = server.available();
-//      serverClient.stop();
-//      Serial1.println("Connection rejected ");
-//    }
-//  }
-//  //check clients for data
-//  for (i = 0; i < MAX_SRV_CLIENTS; i++) {
-//    if (serverClients[i] && serverClients[i].connected()) {
-//      if (serverClients[i].available()) {
-//        //get data from the telnet client and push it to the UART
-//        while (serverClients[i].available()) {
-//          Serial.write(serverClients[i].read());
-//        }
-//      }
-//    }
-//  }
-//  //check UART for data
-//  if (Serial.available()) {
-//    size_t len = Serial.available();
-//    uint8_t sbuf[len];
-//    Serial.readBytes(sbuf, len);
-//    //push UART data to all connected telnet clients
-//    for (i = 0; i < MAX_SRV_CLIENTS; i++) {
-//      if (serverClients[i] && serverClients[i].connected()) {
-//        serverClients[i].write(sbuf, len);
-//        delay(1);
-//      }
-//    }
-//  }
-  
+  webSocket.sendTXT("Owner= "+ owner + ", Data= tmpOp"+"temp"+"nvOil...");
+  delay(1000); 
 }
-
